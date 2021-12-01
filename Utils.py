f3c2b129bb200f66660ea68123cc548edd7b3711
Massimo-Capurro-Llado <massimocapurrollado@gmail.com>
2021-12-01 10:48:46 +0100

    Inizializzata la lettura da file CSV con lettura di colonne utili

--- Utils.py
+++ Utils.py
@@ -2,18 +2,20 @@
 """
 Created on Wed Nov 24 11:06:08 2021
 
-@author: massi
+@authors: Massimo-Capurro-Llado : Massimo Capurro Llado
+          francescocarpineti091299 : Francesco Carpineti 
+          Vadilonga00 : Francesca Vadilonga
 """
 
 import pandas as pd
 import sys
 
-def ReadCSVFile(file):
+#Function to read only desired columns of CSV file
+def ReadCSVFile(file, columns):
     try:
         csvfile= open(file)
-        data = pd.read_csv(csvfile)
+        data = pd.read_csv(csvfile, usecols=columns)
         return data
     except OSError as e:
         print(e)
         sys.exit()
-    
--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 """
 Created on Wed Nov 24 10:19:00 2021
 
-@author: massi
+@authors: Massimo-Capurro-Llado : Massimo Capurro Llado
+          francescocarpineti091299 : Francesco Carpineti 
+          Vadilonga00 : Francesca Vadilonga
 """
 import Utils
 import pandas as pd
 
-data = Utils.ReadCSVFile("indata/yellow_tripdata_2021-03.csv")
-print(data.head(), '\n\n')
+
+
+taxi_cols=['tpep_pickup_datetime', 'PULocationID']
+zone_cols=['LocationID', 'Borough']
+
+
+data = Utils.ReadCSVFile("indata/yellow_tripdata_2021-01.csv", taxi_cols)
 data.info()
-zone_lookup = Utils.ReadCSVFile("indata/taxi+_zone_lookup.csv")
-zone_lookup.index_col="LocationID"
-print(zone_lookup.head())
+zone_lookup = Utils.ReadCSVFile("indata/taxi+_zone_lookup.csv", zone_cols)
+zone_lookup.info()
